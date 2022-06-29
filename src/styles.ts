807cb9d24009cee538d0a3abe876207e3ea747b6
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2022-06-28 21:31:49 -0300

    adding styled-components

---
+++ src/styles.ts
@@ -0,0 +1,162 @@
+import styled from "styled-components";
+
+type Props = {};
+
+export const StyledDiv = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  background-image: url("./Assets/Images/logo_oiti_trans.png");
+  background-position: 20px 20px;
+  background-repeat: no-repeat;
+  text-align: -webkit-center;
+  height: 100%;
+  background-size: 100px;
+  @media (max-width: 37em) {
+    height: 100%;
+    padding: 0px;
+  }
+`;
+
+export const StyledDiv_2 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+`;
+
+export const StyledDiv_3 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+`;
+
+export const StyledDiv_4 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+`;
+
+export const StyledDiv_5 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+`;
+
+export const StyledDiv_6 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  background-color: white;
+  border-radius: 12px;
+  width: 100%;
+  height: 100%;
+  display: block;
+  overflow: hidden;
+  @media (max-width: 37em) {
+    border-radius: initial;
+    width: 100%;
+    height: 100%;
+    background-color: initial;
+    background-repeat: round;
+  }
+`;
+
+export const StyledVideo = styled.video`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  height: 100%;
+  transform: rotateY(180deg);
+  margin-left: -50%;
+  @media (max-width: 37em) {
+    position: relative;
+    z-index: 0;
+    top: 0px;
+    left: 0px;
+    margin-left: 0px;
+    min-width: 100%;
+    min-height: 100%;
+    width: 100%;
+    height: auto;
+    transform: rotateY(180deg);
+  }
+`;
+
+export const StyledDiv_7 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  top: 50%;
+  left: 0px;
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  text-align: -webkit-center;
+  display: inline-block;
+  @media (max-width: 37em) {
+    top: 50%;
+    left: 0px;
+  }
+`;
+
+export const StyledDiv_8 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+`;
+
+export const StyledDiv_9 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 30px;
+  text-shadow: rgba(0, 0, 0, 0.9) 0px 1px 0px;
+  width: 80%;
+  z-index: 2;
+  @media (max-width: 37em) {
+    width: 90%;
+  }
+`;
+
+export const StyledImg = styled.img`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  width: 30%;
+`;
+
+export const StyledSpan = styled.span`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  font-size: 9px;
+  color: white;
+  text-shadow: rgba(0, 0, 0, 0.9) 0px 1px 0px;
+`;
+
+export const StyledDiv_10 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  position: absolute;
+  top: 58%;
+  left: 70px;
+  width: 58%;
+`;
+
+export const StyledImg_2 = styled.img`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  width: 30%;
+`;
+
+export const StyledDiv_11 = styled.div`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+  bottom: 15px;
+  position: absolute;
+  width: 100%;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.button`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+`;
+
+export const StyledCanvas = styled.canvas`
+  box-sizing: border-box;
+  font-family: "Hammersmith One", sans-serif;
+`;
