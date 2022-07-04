7acf660d4a9b9762e39303350c6312e607223f21
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2022-07-03 22:05:24 -0300

    updating version to 0.1.6

--- src/index.tsx
+++ src/index.tsx
@@ -3,64 +3,85 @@ import React from "react";
 import SdkSettings from "./Interfaces/Settings/SdkSettings";
 
 import { IniciarFluxo, IniciarCamera } from "./SdkInitializer";
+import GlobalStyles, {
+  Content,
+  Container,
+  OuterR4x3,
+  Inner,
+  Overlay,
+  ContentVideo,
+  Video,
+  DivLoader,
+  Loader,
+  DivMessage,
+  DivSorriso,
+  DivButton,
+  Button,
+} from "./styles";
 
 interface ComponentInterface {
   settings: SdkSettings;
 }
 
-import {
-  StyledButton,
-  StyledCanvas,
-  StyledDiv,
-  StyledDiv_10,
-  StyledDiv_11,
-  StyledDiv_2,
-  StyledDiv_3,
-  StyledDiv_4,
-  StyledDiv_5,
-  StyledDiv_6,
-  StyledDiv_7,
-  StyledDiv_8,
-  StyledDiv_9,
-  StyledImg,
-  StyledImg_2,
-  StyledSpan,
-  StyledVideo,
-} from "./styles";
+// import {
+//   StyledButton,
+//   StyledCanvas,
+//   StyledDiv,
+//   StyledDiv_10,
+//   StyledDiv_11,
+//   StyledDiv_2,
+//   StyledDiv_3,
+//   StyledDiv_4,
+//   StyledDiv_5,
+//   StyledDiv_6,
+//   StyledDiv_7,
+//   StyledDiv_8,
+//   StyledDiv_9,
+//   StyledImg,
+//   StyledImg_2,
+//   StyledSpan,
+//   StyledVideo,
+// } from "./styles";
 
 const LivenessComponent: React.FC<ComponentInterface> = ({ settings }) => {
   return (
-    <StyledDiv id="content">
-      <StyledDiv_2 id="container">
-        <StyledDiv_3>
-          <StyledDiv_4>
-            <StyledDiv_5 />
+    <>
+      <GlobalStyles />
+      <Content id="content">
+        <Container id="container">
+          <OuterR4x3>
+            <Inner>
+              <Overlay id="overlay" />
 
-            <StyledDiv_6 id="content-video">
-              <StyledVideo id="player" />
-            </StyledDiv_6>
+              <ContentVideo id="content-video">
+                <Video id="player" />
+              </ContentVideo>
 
-            <StyledDiv_7 id="divLoader">
-              <StyledDiv_8 />
-            </StyledDiv_7>
+              <DivLoader id="divLoader" style={{ display: "none" }}>
+                <Loader />
+              </DivLoader>
 
-            <StyledDiv_9 id="divMsg">
-              <StyledImg id="imgMsg" />
-              <StyledSpan id="spanMsg">CLIQUE EM INICIAR</StyledSpan>
-            </StyledDiv_9>
+              <DivMessage id="divMsg" style={{ display: "none" }}>
+                <img id="imgMsg" />
+                <span id="spanMsg">CLIQUE EM INICIAR</span>
+              </DivMessage>
 
-            <StyledDiv_10 id="divSorriso">
-              <StyledImg_2 id="imgChallenge" />
-            </StyledDiv_10>
+              <DivSorriso id="divSorriso" style={{ display: "none" }}>
+                <img id="imgChallenge" />
+              </DivSorriso>
 
-            <StyledDiv_11 id="divButton">
-              <StyledButton id="btnIniciar">INICIAR</StyledButton>
-            </StyledDiv_11>
-          </StyledDiv_4>
-        </StyledDiv_3>
-      </StyledDiv_2>
-      <StyledCanvas id="fc_canvas" />
-    </StyledDiv>
+              <DivButton id="divButton">
+                <Button id="btnIniciar" onClick={() => IniciarFluxo(settings)}>
+                  INICIAR
+                </Button>
+              </DivButton>
+            </Inner>
+          </OuterR4x3>
+        </Container>
+
+        <canvas id="fc_canvas" style={{ display: "none" }}></canvas>
+      </Content>
+    </>
 
     // <div id="content" className="content">
     //   <div id="container" className="container">
--- src/styles.ts
+++ src/styles.ts
@@ -1,49 +1,125 @@
-import styled from "styled-components";
+import styled, { keyframes, createGlobalStyle } from "styled-components";
 
-export const StyledDiv = styled.div`
+export default createGlobalStyle`
+  * {
+  -moz-box-sizing: border-box;
+  -o-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
   box-sizing: border-box;
+
   font-family: "Hammersmith One", sans-serif;
-  background-image: url("./Assets/Images/logo_oiti_trans.png");
-  background-position: 20px 20px;
-  background-repeat: no-repeat;
+}
+
+body {
+  min-height: 100%;
+  height: 100%;
+  padding: 0px;
+  margin: 0px;
+  background-image: url("./Assets/Images/bg_oiti_matrix.png");
+  overflow: hidden;
+  text-align: center;
   text-align: -webkit-center;
+  text-align: -moz-center;
+}
+
+@media (max-width: 37em) {
+  html,
+  body {
+    padding: 0px;
+    margin: 0px;
+    height: 100%;
+  }
+  }
+`;
+
+export const Content = styled.div`
+  background-image: url("./Assets/Images/logo_oiti_trans.png");
+  background-position-x: 20px;
+  background-position-y: 20px;
+  background-repeat: no-repeat;
+  text-align: center;
+  text-align: -webkit-center;
+  text-align: -moz-center;
   height: 100%;
   background-size: 100px;
+
   @media (max-width: 37em) {
     height: 100%;
     padding: 0px;
   }
 `;
 
-export const StyledDiv_2 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const Container = styled.div`
+  text-align: -webkit-center;
+  text-align: -moz-center;
+  text-align: center;
+  height: 100%;
+  width: 320px;
+  display: inline-block;
+
+  @media (max-width: 37em) {
+    width: 100%;
+  }
 `;
 
-export const StyledDiv_3 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const OuterR4x3 = styled.div`
+  position: relative;
+  width: 100%;
+  padding-top: 150%; /* "height" will be 3/4 of width */
+
+  @media (max-width: 37em) {
+    padding-top: 0%; /* "height" will be 3/4 of width */
+    height: 100%;
+    min-height: 100%;
+  }
 `;
 
-export const StyledDiv_4 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const Inner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `;
 
-export const StyledDiv_5 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const Overlay = styled.div`
+  background-image: url(./Assets/Images/mobile_layer-2.1.svg) !important;
+  background-repeat: no-repeat !important;
+  background-size: 100% 100% !important;
+  height: 100%;
+  min-height: 100%;
+  width: 100%;
+  position: absolute;
+  top: auto;
+  left: auto;
+  z-index: 1;
+  opacity: 0.5;
+  border-radius: 0px;
+
+  @media (max-width: 37em) {
+    background-image: url(./Assets/Images/mobile_layer-2.1.svg) !important;
+    background-repeat: no-repeat !important;
+    background-size: 100% 100% !important;
+    height: 100%;
+    min-height: 100%;
+    width: 100%;
+    position: absolute;
+    top: auto;
+    left: auto;
+    z-index: 1;
+    opacity: 0.5;
+    border-radius: 0px;
+  }
 `;
 
-export const StyledDiv_6 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const ContentVideo = styled.div`
   background-color: white;
   border-radius: 12px;
   width: 100%;
   height: 100%;
   display: block;
   overflow: hidden;
+
   @media (max-width: 37em) {
     border-radius: initial;
     width: 100%;
@@ -53,12 +129,7 @@ export const StyledDiv_6 = styled.div`
   }
 `;
 
-export const StyledVideo = styled.video`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
-  height: 100%;
-  transform: rotateY(180deg);
-  margin-left: -50%;
+export const Video = styled.div`
   @media (max-width: 37em) {
     position: relative;
     z-index: 0;
@@ -73,88 +144,124 @@ export const StyledVideo = styled.video`
   }
 `;
 
-export const StyledDiv_7 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const DivLoader = styled.div`
   top: 50%;
   left: 0px;
   position: absolute;
   width: 100%;
   height: 40px;
+  text-align: center;
   text-align: -webkit-center;
+  text-align: -moz-center;
   display: inline-block;
+
   @media (max-width: 37em) {
     top: 50%;
     left: 0px;
   }
 `;
 
-export const StyledDiv_8 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+const load7 = keyframes`  
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
 `;
 
-export const StyledDiv_9 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const Loader = styled.div`
+  color: #aed74e;
+  font-size: 8px;
+  position: relative;
+  text-indent: -9999em;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+
+  :before,
+  :after {
+    border-radius: 53%;
+    width: 2.5em;
+    height: 2.5em;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+    -webkit-animation: ${load7} 1.8s infinite ease-in-out;
+    animation: ${load7} 1.8s infinite ease-in-out;
+    content: "";
+    position: absolute;
+    top: 0;
+  }
+
+  :before {
+    left: -3.5em;
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+
+  :after {
+    left: 3.5em;
+  }
+`;
+
+export const DivMessage = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   color: white;
   font-size: 30px;
-  text-shadow: rgba(0, 0, 0, 0.9) 0px 1px 0px;
+  text-shadow: 0 1px 0 rgba(0, 0, 0, 0.9);
   width: 80%;
   z-index: 2;
+
   @media (max-width: 37em) {
     width: 90%;
   }
 `;
 
-export const StyledImg = styled.img`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
-  width: 30%;
-`;
-
-export const StyledSpan = styled.span`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
-  font-size: 9px;
-  color: white;
-  text-shadow: rgba(0, 0, 0, 0.9) 0px 1px 0px;
-`;
-
-export const StyledDiv_10 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const DivSorriso = styled.div`
   position: absolute;
   top: 58%;
   left: 70px;
   width: 58%;
+
+  img {
+    width: 30%;
+  }
 `;
 
-export const StyledImg_2 = styled.img`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
-  width: 30%;
-`;
-
-export const StyledDiv_11 = styled.div`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+export const DivButton = styled.div`
   bottom: 15px;
   position: absolute;
   width: 100%;
   z-index: 1;
 `;
 
-export const StyledButton = styled.button`
-  box-sizing: border-box;
+export const Button = styled.button`
+  width: 40%;
+  height: 50px;
+  text-decoration: none;
+  text-align: center;
+  font-size: 25px;
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.7);
+  letter-spacing: -0.065em;
   font-family: "Hammersmith One", sans-serif;
-`;
+  -webkit-transition: all 0.25s ease-in-out;
+  -o-transition: all 0.25s ease-in-out;
+  -moz-transition: all 0.25s ease-in-out;
+  transition: all 0.25s ease-in-out;
+  box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.2);
+  border-radius: 50px;
+  z-index: 1;
+  border-width: 4px;
+  border-style: solid;
 
-export const StyledCanvas = styled.canvas`
-  box-sizing: border-box;
-  font-family: "Hammersmith One", sans-serif;
+  background-color: rgb(176, 216, 59);
+  color: rgb(73, 83, 92);
+  border-color: rgba(0, 63, 71, 0.2);
 `;
